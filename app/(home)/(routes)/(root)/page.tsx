6a465d28c4c2b22d1e4d2f66b20ec11c8ca2fc6e
mohammadnedaei <nedaei2002@gmail.com>
2025-03-02 18:46:10 +0330

    feat(home): implement landing page
    Implement initial landing page structure and components.
    
    - Add Hero, Features, Testimonials, and Newsletter components.
    - Introduce feature items with name, description, and icon.
    - Define basic page layout using these components.

--- app/(home)/(routes)/(root)/page.tsx
+++ app/(home)/(routes)/(root)/page.tsx
@@ -1,6 +1,37 @@
+import Hero from "@/app/(home)/_components/hero";
+import Features from "@/app/(home)/_components/features";
+import {CloudIcon, TimerIcon, VerifiedIcon} from "lucide-react";
+import {IFeatureItem} from "@/types/features";
+import Testimonials from "@/app/(home)/_components/testimonials";
+import Newsletter from "@/app/(home)/_components/newsletter";
+const features: IFeatureItem[] = [
+    {
+        name: 'Access Your Interview Collections Anywhere',
+        description:
+            'Store your interview questions and answers securely in the cloud, so you can access them anytime, from any device.',
+        icon: CloudIcon,
+    },
+    {
+        name: 'Verified Pre-Made Interview Sets',
+        description:
+            'Save time with expert-curated interview question sets, covering a variety of industries and roles.',
+        icon: VerifiedIcon,
+    },
+    {
+        name: "Instant Short Quizzes for Quick Practice",
+        description:
+            'Test your knowledge with fast, interactive quizzes designed to reinforce your learning and improve recall.',
+        icon: TimerIcon,
+    },
+];
 const LandingPage = () => {
   return (
-      <></>
+      <>
+        <Hero />
+          <Features features={features} />
+          <Testimonials />
+          <Newsletter />
+      </>
   )
 }
 export default LandingPage
