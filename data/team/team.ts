630f63759c70205d8b5bb3a413992cde1e1a8bea
mohammadnedaei <nedaei2002@gmail.com>
2025-03-11 18:29:08 +0330

    feat(team): add team data and types
    Define Team interface and create initial team member data.
    
    - Introduce `Team` interface for type safety.
    - Add team member data with name, role, and imageUrl.
    - Import Team interface to team data.

---
+++ data/team/team.ts
@@ -0,0 +1,22 @@
+import {Team} from "@/data/types";
+
+export const team: Team[] = [
+    {
+        name: 'Alex Johnson',
+        role: 'Founder / CEO',
+        imageUrl:
+        'https://avatar.iran.liara.run/public/15'
+    },
+    {
+        name: 'Sophia Martinez',
+        role: 'CTO',
+        imageUrl:
+        'https://avatar.iran.liara.run/public/32'
+    },
+    {
+        name: 'Daniel Kim',
+        role: 'COO',
+        imageUrl:
+        'https://avatar.iran.liara.run/public/20'
+    }
+];
