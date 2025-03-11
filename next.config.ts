02b93b5d7271779a3c77d45a7d6dc7f6d5735c91
mohammadnedaei <nedaei2002@gmail.com>
2025-03-11 18:26:49 +0330

    feat(next-config): configure image remote patterns
    Configure next/image to allow images from specific domains
    
    - Allow images from avatar.iran.liara.run
    - Allow images from images.unsplash.com
    - These hosts are used for user avatars and content images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatar.iran.liara.run',
+                pathname: '/public/*',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                pathname: '/*'
+            }
+        ],
+    },
 };
 
 export default nextConfig;
