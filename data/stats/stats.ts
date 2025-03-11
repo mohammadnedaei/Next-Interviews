f1b6719489749ec42de8d4a3b6dd56e33a3ef989
mohammadnedaei <nedaei2002@gmail.com>
2025-03-11 18:28:33 +0330

    feat(data): add stats data and type
    Adds stats data and its type definition.
    
    - Defines the `Stats` interface for representing statistical data.
    - Creates `stats.ts` containing an array of `Stats` objects.
    - Improves data structure for displaying key metrics.

---
+++ data/stats/stats.ts
@@ -0,0 +1,7 @@
+import {Stats} from "@/data/types";
+
+export const stats: Stats[] = [
+    { name: 'Offices worldwide', value: '12' },
+    { name: 'Full-time colleagues', value: '300+' },
+    { name: 'Hours per week', value: '40' },
+]
---
+++ data/types/stats.ts
@@ -0,0 +1,4 @@
+export interface Stats {
+    name: string,
+    value: string
+}
