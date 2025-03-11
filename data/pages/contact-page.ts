8ba7ca2783d78aa840e9bfc08e9eac91d530b41c
mohammadnedaei <nedaei2002@gmail.com>
2025-03-11 18:23:49 +0330

    feat(contact-page): add contact page data
    Add data file for the contact page
    
    - Create `contact-page.ts` to hold page constants
    - Define title and description for SEO and UI
    - Export data for component consumption

---
+++ data/pages/contact-page.ts
@@ -0,0 +1,6 @@
+import {ContactPage} from "@/data/types";
+
+export const contactPageConstants: ContactPage = {
+    title: 'Contact Us',
+    description: 'Reach out to our sales team today and learn how we can help your business grow. We\'re here to answer your questions and provide tailored solutions.'
+}
