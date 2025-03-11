fb884c518dba437c6f643f23d608bb481d0e2fb2
mohammadnedaei <nedaei2002@gmail.com>
2025-03-11 18:27:38 +0330

    feat(about): redesign about page
    Revamp the about page to focus on interview prep.
    
    - Update the layout and content.
    - Add team member section.
    - Integrate new images and icons.

--- app/(home)/(routes)/about/page.tsx
+++ app/(home)/(routes)/about/page.tsx
@@ -1,55 +1,33 @@
-const people = [
-    {
-        name: 'Leslie Alexander',
-        role: 'Co-Founder / CEO',
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-        name: 'Hilary Menard',
-        role: 'Co-Founder / CTO',
-        imageUrl:
-            'https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-        name: 'Chelsea Higard',
-        role: 'Co-Founder / COO',
-        imageUrl:
-            'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-        name: 'Emma Danvers',
-        role: 'Co-Founder / COO',
-        imageUrl:
-            'https://images.unsplash.com/photo-1500648767791-00de80feee70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-
-]
+import {ChevronsLeftRightEllipsis, MonitorSmartphone} from "lucide-react";
+import {team} from "@/data"
+import Image from "next/image";
 const About = () => {
     return (
         <div className="py-8 sm:py-12">
-            <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
-                <h2 className="text-center text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
+            <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8 mb-10">
+                <h2 className="text-center text-base/7 font-semibold text-indigo-600">Crush Your Next Tech Interview</h2>
                 <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance sm:text-5xl">
-                    Everything you need to deploy your app
+                    Master coding interviews with ease
                 </p>
                 <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
                     <div className="relative lg:row-span-2">
-                        <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
+                        <div className="absolute inset-px rounded-lg bg-white dark:bg-zinc-800 lg:rounded-l-[2rem]"></div>
                         <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
                             <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                                    Mobile friendly
+                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 dark:text-primary max-lg:text-center">
+                                    Real Interview Questions
                                 </p>
-                                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 dark:text-primary/70 max-lg:text-center">
+                                    Access a vast collection of real-world coding and system design questions from top companies.
                                 </p>
                             </div>
                             <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
                                 <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
-                                    <img
-                                        className="size-full object-cover object-top"
-                                        src="https://tailwindui.com/plus-assets/img/component-images/bento-03-mobile-friendly.png"
+                                    <Image
+                                        width={400}
+                                        height={650}
+                                        className="size-full object-cover object-bottom"
+                                        src="/images/screenshot/screenshot-mobile.png"
                                         alt=""
                                     />
                                 </div>
@@ -58,91 +36,74 @@ const About = () => {
                         <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 lg:rounded-l-[2rem]"></div>
                     </div>
                     <div className="relative max-lg:row-start-1">
-                        <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
+                        <div className="absolute inset-px rounded-lg bg-white dark:bg-zinc-800 max-lg:rounded-t-[2rem]"></div>
                         <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
                             <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Performance</p>
-                                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.
+                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 dark:text-primary max-lg:text-center">Quizzes & Challenges</p>
+                                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 dark:text-primary/70 max-lg:text-center">
+                                    Test your skills with interactive quizzes and coding challenges.
                                 </p>
                             </div>
                             <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
-                                <img
-                                    className="w-full max-lg:max-w-xs"
-                                    src="https://tailwindui.com/plus-assets/img/component-images/bento-03-performance.png"
-                                    alt=""
-                                />
+                                <ChevronsLeftRightEllipsis size={80} fill={"currentColor"} />
                             </div>
                         </div>
                         <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-t-[2rem]"></div>
                     </div>
                     <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-                        <div className="absolute inset-px rounded-lg bg-white"></div>
+                        <div className="absolute inset-px rounded-lg bg-white dark:bg-zinc-800"></div>
                         <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
                             <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Security</p>
-                                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                                    Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi.
+                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 dark:text-primary max-lg:text-center">Mock Interviews</p>
+                                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 dark:text-primary/70 max-lg:text-center">
+                                    Simulate real interview environments and get AI-powered feedback.
                                 </p>
                             </div>
-                            <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
-                                <img
-                                    className="h-[min(152px,40cqw)] object-cover"
-                                    src="https://tailwindui.com/plus-assets/img/component-images/bento-03-security.png"
-                                    alt=""
-                                />
+                            <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
+                                <MonitorSmartphone size={80} fill={"currentColor"} />
                             </div>
                         </div>
                         <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5"></div>
                     </div>
                     <div className="relative lg:row-span-2">
-                        <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+                        <div className="absolute inset-px rounded-lg bg-white dark:bg-zinc-800 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
                         <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
                             <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                                    Powerful APIs
+                                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 dark:text-primary max-lg:text-center">
+                                    Community & Discussion
                                 </p>
-                                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                                    Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
+                                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 dark:text-primary/70 max-lg:text-center">
+                                    Join a thriving community of learners, share insights, and discuss solutions to tough interview questions. Get tips from experts and improve together!
                                 </p>
                             </div>
-                            <div className="relative min-h-[30rem] w-full grow">
-                                <div className="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
-                                    <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                                        <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
-                                            <div className="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">
-                                                NotificationSetting.jsx
-                                            </div>
-                                            <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
-                                        </div>
-                                    </div>
-                                    <div className="px-6 pt-6 pb-14">{/* Your code example */}</div>
-                                </div>
-                            </div>
+                            <Image
+                                width={400}
+                                height={650}
+                                className="w-11/12 mx-auto mt-15 rounded-xl"
+                                src="/images/icons/support.svg"
+                                alt=""
+                            />
                         </div>
                         <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
                     </div>
                 </div>
             </div>
-            <div className="bg-white py-24 sm:py-32">
+            <div className="py-24 sm:py-32">
                 <div className="mx-auto grid max-w-7xl gap-20 px-6 lg:px-8 xl:grid-cols-3">
                     <div className="max-w-xl">
-                        <h2 className="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">
-                            Meet our leadership
-                        </h2>
-                        <p className="mt-6 text-lg/8 text-gray-600">
-                            We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the
-                            best results for our clients.
+                        <h2 className="text-3xl font-semibold tracking-tight text-gray-900 dark:text-zinc-100 sm:text-4xl">Meet Our Team</h2>
+                        <p className="mt-6 text-lg text-gray-600 dark:text-zinc-300">
+                            Passionate engineers and educators, dedicated to making tech interviews easier for everyone.
                         </p>
                     </div>
                     <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
-                        {people.map((person) => (
+                        {team.map((person) => (
                             <li key={person.name}>
                                 <div className="flex items-center gap-x-6">
-                                    <img alt="" src={person.imageUrl} className="size-16 rounded-full" />
+                                    <Image width={50} height={50} alt="team-item" src={person.imageUrl} className="size-16 rounded-full" />
                                     <div>
-                                        <h3 className="text-base/7 font-semibold tracking-tight text-gray-900">{person.name}</h3>
-                                        <p className="text-sm/6 font-semibold text-indigo-600">{person.role}</p>
+                                        <h3 className="text-base/7 font-semibold tracking-tight text-gray-900 dark:text-zinc-300">{person.name}</h3>
+                                        <p className="text-sm/6 font-semibold text-indigo-500">{person.role}</p>
                                     </div>
                                 </div>
                             </li>
